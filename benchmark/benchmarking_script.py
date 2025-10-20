41b5b749fe428beb2011190e0cae69b2bd8aa42d
Tiekai Bi <tiekaib@nvidia.com>
2025-10-21 00:01:36 +0800

    add annotated attn
    Signed-off-by: Tiekai Bi <tiekaib@nvidia.com>

--- benchmark/benchmarking_script.py
+++ benchmark/benchmarking_script.py
@@ -6,6 +6,11 @@ import timeit
 import torch
 import torch.cuda.nvtx as nvtx
 
+from jaxtyping import Float, Bool
+from einops import einsum
+from torch import Tensor
+from cs336_basics.nn_utils import softmax
+
 from typing import Callable
 
 def benchmark_model(description: str, num_warmup_iters: int, num_iters: int, function: Callable, *args, **kwargs) -> tuple[float, float]:
@@ -57,6 +62,46 @@ def benchmark_train_step(description: str, num_warmup_iters: int, num_iters: int
                     param_group["lr"] = lr
                 optimizer.step()
 
+@nvtx.range("scaled_dot_product_attention")
+def annotated_scaled_dot_product_attention(
+    Q: Float[Tensor, " ... queries d_k"],
+    K: Float[Tensor, " ... keys    d_k"],
+    V: Float[Tensor, " ... keys    d_v"],
+    mask: Bool[Tensor, " ... queries keys"] | None = None,
+) -> Float[Tensor, " ... queries d_v"]:
+    """Scaled dot-product attention.
+
+    This function implements Eq. 1 of the Transformer paper.
+
+    Args:
+        Q: Tensor of queries, may have any number of leading dimensions.
+        K: Tensor of keys, sharing leading dimensions with Q.
+        V: Tensor of values, sharding leading dimensions with Q and K.
+        mask: An (optional) mask of shape (..., seq_len, seq_len).
+            Attention scores for positions with a mask value of `False` should
+            be masked out, i.e., not affect the softmaxed attention probabilities.
+
+    Returns:
+        torch.FloatTensor of shape (..., seq_len, value_dimension)
+        with the output of running your scaled dot product attention
+        implementation with the provided key, query, and value tensors.
+    """
+
+    d_k = K.shape[-1]
+    with nvtx.range("attention_scores"):
+        attention_scores = einsum(Q, K, "... query d_k, ... key d_k -> ... query key") / math.sqrt(d_k)
+    with nvtx.range("attention_scores_mask"):
+        if mask is not None:
+            attention_scores = torch.where(mask, attention_scores, float("-inf"))
+
+    with nvtx.range("computing softmax"):
+        attention_weights = softmax(attention_scores, dim=-1)  # Softmax over the key dimension
+
+    with nvtx.range("computing output"):
+        result = einsum(attention_weights, V, "... query key, ... key d_v ->  ... query d_v")
+
+    return result
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -75,6 +120,7 @@ def parse_args() -> argparse.Namespace:
 
 def main():
     args = parse_args()
+    cs336_basics.model.scaled_dot_product_attention = annotated_scaled_dot_product_attention
     model: cs336_basics.model.BasicsTransformerLM = cs336_basics.model.BasicsTransformerLM(
         vocab_size=args.vocab_size,
         context_length=args.context_length,
